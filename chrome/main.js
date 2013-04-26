0e422d7d6f69d8ea2eedd7f60c35e4fa7180beab
Ian Webster <ian@room77.com>
2013-04-26 16:45:08 -0700

    slightly organized

--- chrome/main.js
+++ chrome/main.js
@@ -3,40 +3,52 @@ $(function() {
     var hash = location.hash;
     var match = hash.match(/^#inbox\/(.+)$/g);
     if (match) {
-      // threaded view
+      // We're in a threaded view
 
+      // Make sure the page has loaded
       var $morebtn = $('.Ykrj7b:last').parent();
       if ($morebtn.length < 1) {
-        console.log('page still loading. retrying...');
         setTimeout(HashChanged, 300);
         return;
       }
 
-      var gmail_globals = [];
-      var $globals_include = $('#hist_state').next();
-      if ($globals_include.prop('tagName') === 'SCRIPT') {
-        (function() {
-          // shame on me
-          try{
-            eval($globals_include.text());
-          } catch(ex) {}
-          gmail_globals = GLOBALS;
-        })();
+      // Various helper functions
+
+      var getMessageId = function(callback) {
+        var gmail_globals = [];
+        var $globals_include = $('#hist_state').next();
+        if ($globals_include.prop('tagName') === 'SCRIPT') {
+          (function() {
+            // shame on me
+            try{
+              eval($globals_include.text());
+            } catch(ex) {}
+            gmail_globals = GLOBALS;
+          })();
+        }
+
+        var gmail_ik = gmail_globals[9];
+        var gmail_th = $('.ii:not(.undefined):last').attr('class').match(/m([^ ]+)/)[1];
+
+        var details_url = document.location.host + document.location.pathname
+              + '?ui=2&ik=' + gmail_ik + '&view=om&th=' + gmail_th;
+        $.get(details_url, function(data) {
+          var message_id = data.match(/Message-ID: <([^>]+)/)[1];
+          console.log('message_id', message_id);
+          callback(message_id);
+        });
       }
 
-      var gmail_ik = gmail_globals[9];
-      var gmail_th = $('.ii:not(.undefined):last').attr('class').match(/m([^ ]+)/)[1];
+      var cancelDialog = function() {
+        $mask.remove();
+        $dialog.remove();
+      }
 
-      var details_url = document.location.host + document.location.pathname
-            + '?ui=2&ik=' + gmail_ik + '&view=om&th=' + gmail_th;
-      $.get(details_url, function(data) {
-        var message_id = data.match(/Message-ID: <([^>]+)/)[1];
-        console.log('message_id', message_id);
-      });
-
+      // Build DOM
       $morebtn.parent()
         .append('<div id="bump_later" class="T-I J-J5-Ji ar7 nf T-I-ax7 L3"><span>Bump Later</span></div>')
         .on('click', function() {
+          // Show dialog
           var $dialog = $('<div>').css({
             'position': 'fixed',
             'left': '50%',
@@ -63,11 +75,7 @@ $(function() {
 
           $dialog.appendTo('body');
 
-          var cancelDialog = function() {
-            $mask.remove();
-            $dialog.remove();
-          }
-
+          // Background mask
           var $mask = $('<div>').css({
             'position': 'fixed',
             'left': 0,
@@ -78,7 +86,13 @@ $(function() {
             'background-color': '#000',
             'opacity': .7
           }).appendTo('body').on('click', cancelDialog);
-        });
+
+          // Grab message id
+          var mid;
+          getMessageId(function(x) {
+            mid = x;
+          });
+        });  // end onclick
     }
   }
 
