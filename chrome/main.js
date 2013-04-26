d3b8e8ea1152e2ba62e159f80b9a93a548715f5a
Ian Webster <ian@room77.com>
2013-04-26 16:32:39 -0700

    pulling message id

--- chrome/main.js
+++ chrome/main.js
@@ -3,21 +3,36 @@ $(function() {
     var hash = location.hash;
     var match = hash.match(/^#inbox\/(.+)$/g);
     if (match) {
-      console.log('threaded view');
       // threaded view
 
       var $morebtn = $('.Ykrj7b:last').parent();
       if ($morebtn.length < 1) {
-        console.log('failed?');
+        console.log('page still loading. retrying...');
         setTimeout(HashChanged, 300);
         return;
       }
 
-      if (typeof GLOBALS !== 'undefined') {
-        var gmail_ik = GLOBALS[9];
-        console.log('ik', gmail_ik);
+      var gmail_globals = [];
+      var $globals_include = $('#hist_state').next();
+      if ($globals_include.prop('tagName') === 'SCRIPT') {
+        (function() {
+          // shame on me
+          try{
+            eval($globals_include.text());
+          } catch(ex) {}
+          gmail_globals = GLOBALS;
+        })();
       }
-      console.log(document.getElementById(':oz'));
+
+      var gmail_ik = gmail_globals[9];
+      var gmail_th = $('.ii:not(.undefined):last').attr('class').match(/m([^ ]+)/)[1];
+
+      var details_url = document.location.host + document.location.pathname
+            + '?ui=2&ik=' + gmail_ik + '&view=om&th=' + gmail_th;
+      $.get(details_url, function(data) {
+        var message_id = data.match(/Message-ID: <([^>]+)/)[1];
+        console.log('message_id', message_id);
+      });
 
       $morebtn.parent()
         .append('<div id="bump_later" class="T-I J-J5-Ji ar7 nf T-I-ax7 L3"><span>Bump Later</span></div>')
@@ -48,6 +63,11 @@ $(function() {
 
           $dialog.appendTo('body');
 
+          var cancelDialog = function() {
+            $mask.remove();
+            $dialog.remove();
+          }
+
           var $mask = $('<div>').css({
             'position': 'fixed',
             'left': 0,
@@ -57,10 +77,7 @@ $(function() {
             'z-index': 9998,
             'background-color': '#000',
             'opacity': .7
-          }).appendTo('body').on('click', function() {
-            $mask.remove();
-            $dialog.remove();
-          });
+          }).appendTo('body').on('click', cancelDialog);
         });
     }
   }
