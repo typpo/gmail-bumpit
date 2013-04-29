da1f4c4ae3e06fd7819338fc186bb97b71f74449
Ian Webster <ian@room77.com>
2013-04-29 14:22:57 -0700

    setup improvements

--- scripts/first_time.sh
+++ scripts/first_time.sh
@@ -7,13 +7,26 @@ cd ..
 command -v heroku >/dev/null 2>&1 || { echo >&2 "Heroku toolkit needs to be installed (https://toolbelt.heroku.com/). Aborting."; exit 1; }
 
 heroku login
-heroku addons:add redistogo
-heroku addons:add scheduler:standard
-heroku addons:add newrelic:standard   # pings to keep app from idling on heroku's free tier
 
-randomstr=`tr -dc "[:alpha:]" < /dev/urandom | head -c 8`
-heroku apps:create "gmail-bumper-$randomstr"
-git push heroku master
+randomstr=`tr -dc "a-z0-9" < /dev/urandom | head -c 8`
+appname="gmail-bumper-$randomstr"
+
+function failed {
+  echo "!!! Setup failed.  Please fix and try again."
+  echo "Removing the app we created..."
+  heroku apps:destroy --app $appname --confirm $appname
+  git remote rm heroku >/dev/null 2>&1
+  echo "Done."
+  exit 1
+}
+
+echo "Creating app $appname.."
+heroku apps:create $appname
+heroku addons:add redistogo || failed
+heroku addons:add scheduler:standard || failed
+heroku addons:add newrelic:standard || failed  # pings to keep app from idling on heroku's free tier
+
+git push heroku master || failed
 
 heroku ps:scale web=1
 
